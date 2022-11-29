995f52cc1b059baf25840d0107823493df4db6c7
Ezequiel Amin <ezequielamin@outlook.com>
2022-11-28 21:10:37 -0300

    agrego env y json server

--- src/components/Main/ProductGrid/ProductGrid.jsx
+++ src/components/Main/ProductGrid/ProductGrid.jsx
@@ -1,57 +1,27 @@
+import axios from 'axios';
+import { useEffect, useState } from 'react';
 import { Col, Row } from 'react-bootstrap';
 import ProductItem from '../ProductItem/ProductItem';
 
-const fakeArray = [
-  {
-    id: 1,
-    name: 'Zanahoria',
-    price: 25.23,
-    description: 'Vegetal copado',
-    image: "https://img.freepik.com/fotos-premium/zanahoria-entera-aislada_88281-1988.jpg?w=2000"
-  },
-  {
-    id: 2,
-    name: 'Zanahoria',
-    price: 25.23,
-    description: 'Vegetal copado',
-    image: "https://img.freepik.com/fotos-premium/zanahoria-entera-aislada_88281-1988.jpg?w=2000"
-  },
-  {
-    id: 3,
-    name: 'Zanahoria',
-    price: 25.23,
-    description: 'Vegetal copado',
-    image: "https://img.freepik.com/fotos-premium/zanahoria-entera-aislada_88281-1988.jpg?w=2000"
-  },
-  {
-    id: 4,
-    name: 'Zanahoria',
-    price: 25.23,
-    description: 'Vegetal copado',
-    image: "https://img.freepik.com/fotos-premium/zanahoria-entera-aislada_88281-1988.jpg?w=2000"
-  },
-  {
-    id: 5,
-    name: 'Zanahoria',
-    price: 25.23,
-    description: 'Vegetal copado',
-    image: "https://img.freepik.com/fotos-premium/zanahoria-entera-aislada_88281-1988.jpg?w=2000"
-  },
-  {
-    id: 6,
-    name: 'Zanahoria',
-    price: 25.23,
-    description: 'Vegetal copado',
-    image: "https://img.freepik.com/fotos-premium/zanahoria-entera-aislada_88281-1988.jpg?w=2000"
-  },
-];
+const baseUrl = process.env.REACT_APP_BASE_URL;
 
 const ProductGrid = () => {
+  const [items, setItems] = useState([]);
+  
+  useEffect(() => {
+    const itemsFetch = async () => {
+      const data = await axios.get(`${baseUrl}/items`);
+      setItems(data.data);
+    };
+
+    itemsFetch();
+  }, []);
+
   return (
     <Row>
-      {fakeArray.map((elemento) => {
+      {items.map((elemento) => {
         return (
-          <Col xs={12} md={4} lg={3} key={elemento.id} className="p-2">
+          <Col xs={12} md={4} lg={3} key={elemento.id} className='p-2'>
             {/* <ProductItem
               id={elemento.id}
               name={elemento.name}
@@ -60,7 +30,7 @@ const ProductGrid = () => {
               image={elemento.image}
             /> */}
             {/* Version reducida v */}
-            <ProductItem {...elemento} /> 
+            <ProductItem {...elemento} />
           </Col>
         );
       })}
--- src/views/Main/MainView.jsx
+++ src/views/Main/MainView.jsx
@@ -1,9 +1,12 @@
 import { Container } from 'react-bootstrap';
+
 import ProductGrid from '../../components/Main/ProductGrid/ProductGrid';
 
 const MainView = () => {
   return (
     <Container className='my-5'>
+      <h1 className='text-center'>Bienvenidos a Rolling Market</h1>
+      <hr />
       <ProductGrid />
     </Container>
   );
